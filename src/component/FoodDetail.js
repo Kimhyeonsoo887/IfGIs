aba9caeeaeacca82851cffc53795c8239ad48ca1
김현수 <rlagustn8705@naver.com>
2023-05-09 17:47:02 +0900

    리뷰등록 모달창 및 파일업로드 로직

--- src/component/FoodDetail.js
+++ src/component/FoodDetail.js
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
+import FoodReviewPop from './popupComponent/FoodReviewPop';
 
 export default function FoodDetail(){
 
 
     const {id} = useParams();
+    const [isPopupBoo, setPopupBoo] = useState(false);
 
     const [foodDetailData, foodDetailDispatch] = useReducer(reducer,{
         storeName : "",
@@ -48,29 +50,43 @@ export default function FoodDetail(){
         if(foodDetailData.storeName === ""){
             getFoodDetailData();
         }
-
-       
-
-        
-
     },[foodDetailData])
 
 
+    function openPopUp(){
+        setPopupBoo(true);
+    }
 
+    function closePopUp(){
+        setPopupBoo(false);
+    }
 
     return (
 
-        <form style={{width:100+"%", height:700+"px"}}>
-
+        <>
             <div>
-                <label>상세화면</label> <br/>
-                <label>가게이름</label><input type="text" style={{marginTop:10+"px"}} defaultValue={foodDetailData.storeName} /><br/>
-                <label>주소</label><input type="text" style={{marginTop:5+"px"}} defaultValue={foodDetailData.address} /><br/>
-                <label>가게형태</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.storeType}/><br/>
-                <label>결제방식</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.paymentMtd}/><br/>
-                <label>출몰시기</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.appTime}/><br/>
-                <label>카테고리</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.category}/><br/>
+                <form style={{width:100+"%", height:400+"px"}}>
+
+                    <div>
+                        <label>상세화면</label> <br/>
+                        <label>가게이름</label><input type="text" style={{marginTop:10+"px"}} defaultValue={foodDetailData.storeName} disabled/><br/>
+                        <label>주소</label><input type="text" style={{marginTop:5+"px", marginLeft:27+"px"}} defaultValue={foodDetailData.address} disabled/><br/>
+                        <label>가게형태</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.storeType} disabled/><br/>
+                        <label>결제방식</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.paymentMtd} disabled/><br/>
+                        <label>출몰시기</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.appTime} disabled/><br/>
+                        <label>카테고리</label><input type="text"  style={{marginTop:5+"px"}} defaultValue={foodDetailData.category} disabled/><br/>
+                    </div>
+                </form>
             </div>
-        </form>
+
+
+
+            <div style={{width:100+"%", height:200+"px", border:1+"px solid #e0e0e0"}}>
+                <button onClick={openPopUp}>리뷰 등록하기</button>
+                등록된 리뷰가 없습니다.
+            </div>
+
+            {isPopupBoo ? <FoodReviewPop open={openPopUp} close={closePopUp} header="리뷰 작성"></FoodReviewPop> : ""}
+        </>
     )
 }
---
+++ src/component/popupComponent/FoodReviewPop.js
@@ -0,0 +1,121 @@
+
+import React, { useState, useReducer } from 'react';
+import '../../css/Modal.css';
+import axios from 'axios';
+
+const FoodReviewPop = (props) => {
+
+    const { open, close, header } = props;
+    const [imageFile, setImageFile] = useState("/images/notImages.png");
+    const [formImageData, setFormImageData] = useState();
+
+    const [reviewData, reviewDataDispatch] = useReducer(reducer,{
+        title : "",
+        content: ""
+    })
+
+    function reducer(state, action){
+        return{ 
+            ...state,
+            [action.name]: action.value
+        };
+    }
+
+    function handleChange(e){
+        reviewDataDispatch(e.target);
+    }
+
+    function showImageFile(e){
+        const file = e.target.files[0];
+        //파일선택창 선택없이 닫을경우 예외처리 로직
+        if(file === undefined){
+            setImageFile("/images/notImages.png");
+            return;
+        }
+        
+        setFormImageData(file);
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        return new Promise((resolve) => { 
+            reader.onload = () => {	
+                setImageFile(reader.result || null); // 파일의 컨텐츠
+                resolve();
+            };
+        });
+
+        
+        
+    }
+
+    function insertReview(){
+
+
+        if(reviewData.title === ""){
+            alert("제목을 입력하세요");
+            return;
+        }
+
+        if(reviewData.content === ""){
+            alert("내용을 입력하세요");
+            return;
+        }
+
+        
+        const formData = new FormData();
+
+
+        formData.append("title", reviewData.title);
+        formData.append("content", reviewData.content);
+        formData.append("file", formImageData);
+
+        axios({
+            url: 'http://183.109.96.235:8080/insertFoodReview', // 통신할 웹문서
+            method: 'POST', // 통신할 방식
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" }
+          }).then(function (res) {
+
+            if(res.data !== 0){
+                alert("등록되었습니다.");
+                close();
+            }
+            
+
+            console.log(res.data);
+          }).catch((error => {
+            console.log(`error: ${error}`)
+          }));
+    }
+
+
+
+    return (
+
+        // 모달이 열릴때 openModal 클래스가 생성된다.
+        <div className={open ? 'openModal modal' : 'modal'}>
+        {open ? (
+        <section>
+            <header>
+                {header}
+                <button className="close" onClick={close}>&times;</button>
+            </header>
+            <main>
+                <div>
+                    <label>제목</label><input type="text" name="title" onChange={(e) => {handleChange(e)}}></input><br/>
+                    <label>내용</label><input type="text" name="content" onChange={(e) => {handleChange(e)}}></input>
+                    <input type='file' name="file" accept='image/png, image/jpg, image/jpeg' onChange={e => {showImageFile(e)}}></input>
+                    <img width={'200px'} src={imageFile} />
+                </div>
+            </main>
+            <footer>
+                <button className="close" onClick={insertReview}>제출</button>
+                <button className="close" onClick={close}>닫기</button>
+            </footer>
+        </section>
+        ) : null}
+    </div>
+    );
+};
+
+export default FoodReviewPop;
